50773b676e9d03fd6b030d51f85b4b0e38beba43
venkatesan20 <venkatesannaveen976@gmail.com>
2026-07-09 14:16:14 +0530

    updated style

--- src/Components/NavComponent.jsx
+++ src/Components/NavComponent.jsx
@@ -2,28 +2,41 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import BootstrapNavbar from "react-bootstrap/Navbar";
-import { HiOutlineCodeBracket } from "react-icons/hi2";
 import logo from "../assets/vr-logo.png";
-
-
-
 
 const NavComponent = () => {
   return (
-    <BootstrapNavbar className="custom-navbar">
+    <BootstrapNavbar
+      expand="lg"
+      fixed="top"
+      className="custom-navbar"
+    >
       <Container>
-        <BootstrapNavbar.Brand href="/">
+
+        <BootstrapNavbar.Brand href="#home">
           <img src={logo} alt="VR Logo" className="logo-img" />
         </BootstrapNavbar.Brand>
 
-        <Nav className="ms-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#skills">Skills</Nav.Link>
-          <Nav.Link href="#tools">Tools</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <BootstrapNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+
+            <Nav.Link href="#home">Home</Nav.Link>
+
+            <Nav.Link href="#about">About</Nav.Link>
+
+            <Nav.Link href="#skills">Skills</Nav.Link>
+
+            <Nav.Link href="#tools">Tools</Nav.Link>
+
+            <Nav.Link href="#projects">Projects</Nav.Link>
+
+            <Nav.Link href="#contact">Contact</Nav.Link>
+
+          </Nav>
+        </BootstrapNavbar.Collapse>
+
       </Container>
     </BootstrapNavbar>
   );
